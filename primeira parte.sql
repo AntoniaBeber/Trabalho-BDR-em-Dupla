1c90d4d969e7c37f10d5c290b74b2819cc31bd1f
Antonia Beber <86435535+AntoniaBeber@users.noreply.github.com>
2024-04-13 23:25:51 -0300

    Update primeira parte.sql

--- primeira parte.sql
+++ primeira parte.sql
@@ -9,16 +9,29 @@ precoCusto FLOAT(8,2),
 categoria int
 );
 
+CREATE TABLE Produtos(
+codigo int primary key,
+descricao varchar(50),
+precoVenda FLOAT,
+precoCusto FLOAT,
+categoria int
+);
+
 INSERT into Produtos ()
 VALUES 
-('Caderno','5.45','1.0',1),
-('Caneta','1.20','0.50',1),
-('CD','1.0','0.10',2),
-('Mouse','14.00','5.0',2),
-('Borracha','1','0.5',1),
-('Grampeador','15.00','7.00',1),
-('Carregador','20.00','10.50',2),
-('Chaveiro','5.00', '1.25',3),
-('Bola de Isopor','15.00','7.00',1),
-('Livro', '20.00','11.00',2),
-('Apagador','7.50','2.30',1);
+('1', 'Caderno','5.45','1.0',1),
+('2', 'Caneta','1.20','0.50',1),
+('3', 'CD','1.0','0.10',2),
+('4', 'Mouse','14.00','5.0',2),
+('5', 'Borracha','1.0','0.5',1),
+('6', 'Grampeador','15.00','7.00',1),
+('7', 'Carregador','20.00','10.50',2),
+('8', 'Chaveiro','5.00', '1.25',3),
+('9', 'Bola de Isopor','15.00','7.00',1),
+('10', 'Livro', '20.00','11.00',2),
+('11', 'Apagador','7.50','2.30',1);
+
+SELECT 
+SUM(precoVenda)
+from
+Produtos
